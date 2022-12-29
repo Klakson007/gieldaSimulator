767af24080f50c74f693d671cf92ec1511959665
Hubcius <hubert.tubis@gmail.com>
2022-12-29 15:19:51 +0100

    l

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import pandas as pd
 import time
 import customtkinter
 
-customtkinter.set_appearance_mode("Dark")  
-customtkinter.set_default_color_theme("blue")
+customtkinter.set_appearance_mode("light") 
+customtkinter.set_default_color_theme("blue") 
 current_time = int(time.time())
 start_time = current_time - 60*60*24*14  
 end_time = current_time 
@@ -29,10 +29,18 @@ canvas = FigureCanvasTkAgg(fig, window)
 canvas.get_tk_widget().pack(side='right', fill='y',ipadx=400)
 window.mainloop()
 '''
-self.title("CustomTkinter complex_example.py")
-self.geometry(f"{1100}x{580}")
 
-        # configure grid layout (4x4)
-self.grid_columnconfigure(1, weight=1)
-self.grid_columnconfigure((2, 3), weight=0)
-self.grid_rowconfigure((0, 1, 2), weight=1)
+
+app = customtkinter.CTk()
+app.geometry("400x780")
+app.title("EUR/USD")
+#app.color='#FAEBD7'
+plt.style.use(['Solarize_Light2'])
+fig, ax = plt.subplots()
+ax.plot(data['Date'], data['Close'])
+ax.set_xlabel('Date')
+ax.set_ylabel('Exchange rate')
+
+canvas = FigureCanvasTkAgg(fig, app)
+canvas.get_tk_widget().pack(side='right', fill='y',ipadx=400)
+app.mainloop()
