7d493b7ef4576e2aa2c8d70c22945f257caa1ce0
Hubcius <hubert.tubis@gmail.com>
2022-12-31 14:40:37 +0100

    l

--- main2.py
+++ main2.py
@@ -1,13 +1,16 @@
 import yfinance as yf
-
+import string
+print("sell")
 # Get the EUR/USD currency pair
 eur_usd = yf.Ticker("EURUSD=X")
-l=eur_usd
-
+info = eur_usd.info
+print("sell")
+buy_price = info['regularMarketPrice']
+sell_price = str(info['regularMarketPreviousClose'])
+print("sell")
 # Get the historical data for the previous week
 historical_data = eur_usd.history(period="1wk")
-
+print("sell")
 # Get the closing price for the previous week
 closing_price = historical_data["Close"][-1]
-
-print(f"The closing price of EUR/USD for the previous week was {closing_price}")
+str(sell_price)
